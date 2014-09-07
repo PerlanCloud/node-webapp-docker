6b057151da1eedc1480447200f56a7e2b0cddca8
Perlan Cloud <perlancloud@gmail.com>
2014-09-07 18:25:34 +0200

    Using WEB_NODE to name instances

--- index.js
+++ index.js
@@ -5,12 +5,15 @@ var util = require('util');
 // Constants
 var PORT = 8080;
 
+// Env
+var WEB_NODE = process.env.WEB_NODE || 'WEB_NODE';
+
 // App
 var app = express();
 app.get('/', function (req, res) {
-  res.send('Node.js server is running on Host: ' + os.hostname() + '\n' +
+  res.send('Node.js server ' + + ' is running on Host: ' + os.hostname() + '\n' +
            'List of network interfaces:\n' + util.inspect(os.networkInterfaces()) + '\n');
 });
 
 app.listen(PORT)
-console.log('Running on http://localhost:' + PORT);
+console.log('Running on ' + WEB_NODE + ' http://localhost:' + PORT);
