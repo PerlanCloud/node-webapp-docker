0273c63771198ef3b06ac0aab84e7ce0000ee39e
Perlan Cloud <perlancloud@gmail.com>
2014-09-07 18:05:17 +0200

    Show infos about hostname and network interfaces

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var os = require('os');
+var util = require('util');
 
 // Constants
 var PORT = 8080;
@@ -6,7 +8,8 @@ var PORT = 8080;
 // App
 var app = express();
 app.get('/', function (req, res) {
-  res.send('Node.js server is running.\n');
+  res.send('Node.js server is running on Host: ' + os.hostname() + '\n' +
+           'List of network interfaces:\n' + util.inspect(os.networkInterfaces()) + '\n');
 });
 
 app.listen(PORT)
